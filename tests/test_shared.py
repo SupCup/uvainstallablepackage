1c76f8638a6f1da423275771509c81375bf9789d
Ozioma Ofoma <57122911+SupCup@users.noreply.github.com>
2023-02-23 01:28:30 -0600

    Update test_shared.py

--- tests/test_shared.py
+++ tests/test_shared.py
@@ -1,8 +1,28 @@
 import sys
 sys.path.append('.')
 import shared as sh
+import pytest
 
 def test_clean_string():
     test_str = " This! is      a ,test string  "
 
     assert "This is a test string" == sh.clean_string(test_str), "String <{}> not cleaned as expected".format(test_str)
+
+def test_assertion():
+    assert 1 == 1
+   
+
+@pytest.mark.xfail
+def test_failing():
+    assert 1 == 2
+    
+@pytest.mark.skip(reason = "Because I said so")
+def test_skip():
+    assert "Green Eggs and Ham" == "Sam I am"
+    
+@pytest.mark.skipif(sys.platform == "windows", reason="This does not work on my platform")
+def test_onlyplatform():
+
+    print(sys.platform)
+    assertion_value = 3
+    assert 2 == assertion_value, "Wrong value"
